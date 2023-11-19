c48cca7bef215383b920079c92ecee3a5bfb1c92
daridll <145256722+daridll@users.noreply.github.com>
2023-11-19 22:28:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let x = 4; 
+let y = 5;
+let z = x + y;
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+var num = 6;
+var result = "Result: "
+console.log(result + num + "!")
+/////////////////////////////////////////////
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+function print(word){
+    console.log(word)
+    console.log("!")
+}
+
+
+function add(x,y){
+    let res = x + y;
+    console.log("Результат: "+ res)
+}
+
+add(6,8)
+/////////////////////////////////////////////////
+function sum(a, b) {
+    let result = a + b;
+    return result;   
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+let sumRes = sum(12, 8)
+console.log(sumRes)
